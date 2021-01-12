9199e270f20d3747d11bbea094c78afb4a73c474
Auri Munoz <amunozhe@redhat.com>
2021-01-12 22:13:07 +0100

    Add readme and clean controller

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 
-import io.dekorate.kubernetes.annotation.KubernetesApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
