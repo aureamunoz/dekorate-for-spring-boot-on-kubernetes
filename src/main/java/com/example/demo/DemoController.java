e576602a94586580798707c83354220711cd84b9
Auri Munoz <amunozhe@redhat.com>
2021-01-12 22:36:11 +0100

    configurate dekorate

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
 
+import io.dekorate.kubernetes.annotation.KubernetesApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@KubernetesApplication(replicas = 3)
 @RestController
 public class DemoController {
 
